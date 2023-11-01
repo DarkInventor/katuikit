5155c14def553b8ad9beee7b12f255923972e0ac
DarkInventor <ktmehta25@gmail.com>
2023-11-02 04:51:07 +0530

    frontend almost ready

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title> KatUI | Collection of Components + templates built using React</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Collection of React + Tailwind Based Components and Templates" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="icon" href="/images/dark-logo.ico" sizes="any" />
     </>
   );
 }
